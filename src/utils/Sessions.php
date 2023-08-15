9e6d1ffd0a6df748eef3dbf12badf43e82febbcd
itlattice <zqu1016@126.com>
2023-08-15 21:49:24 +0800

    信息

--- src/utils/Sessions.php
+++ src/utils/Sessions.php
@@ -6,14 +6,7 @@ trait Sessions
 {
     public function AddSession($number=1){
         $time=strtotime($this->time);
-        $str='';
-        if($number>0){
-            $str="+ {$number} minutes";
-        } else{
-            $str="- {$number} minutes";
-        }
-        $time=strtotime($str,$time);
-        $this->time=date('Y-m-d H:i:s',$time);
+        $this->time=date('Y-m-d H:i:s',$time+$number);
         return $this;
     }
 }
